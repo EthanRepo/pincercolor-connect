0495fa8033ad67c64394ca45c63e70664e2f3e81
EthanRepo <100090947+EthanRepo@users.noreply.github.com>
2022-05-12 15:37:36 -0700

    Update script.js

--- connect/script.js
+++ connect/script.js
@@ -1,12 +1,36 @@
-// var x = 0;
+var x = 0;
 function cdFunction() {
   document.getElementById('connect5').style.display = "block";
   document.getElementById('connectstart').style.display = "none";
-  const FivetoFourTimeout = setTimeout(cdBody, 1000);
-// x = 1;
-}
+  const FivetoFourTimeout = setTimeout(cdFive, 1000);
 
-function cdBody() {
+}
+function cdFive() {
   document.getElementById("connect5").style.display = "none"
   document.getElementById("connect4").style.display = "block"
+  const FivetoFourTimeout = setTimeout(cdFour, 1000);
+}
+function cdFour() {
+  document.getElementById("connect4").style.display = "none"
+  document.getElementById("connect3").style.display = "block"
+  const FivetoFourTimeout = setTimeout(cdThree, 1000);
+}
+function cdThree() {
+  document.getElementById("connect3").style.display = "none"
+  document.getElementById("connect2").style.display = "block"
+  const FivetoFourTimeout = setTimeout(cdTwo, 1000);
+}
+function cdTwo() {
+  document.getElementById("connect2").style.display = "none"
+  document.getElementById("connect1").style.display = "block"
+  const FivetoFourTimeout = setTimeout(cdDone, 1000);
+}
+function cdDone() {
+if (x === 1) {
+  document.getElementById("connecty").style.display = "block"
+document.getElementById("connect1").style.display = "none"
+} else {
+document.getElementById("connectn").style.display = "block"
+  document.getElementById("connect1").style.display = "none"
+}
 }
