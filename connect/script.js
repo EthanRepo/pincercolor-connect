1fc8e2f87d56fd7e4080277ada75f342d5f29ac8
EthanRepo <100090947+EthanRepo@users.noreply.github.com>
2022-05-12 10:06:51 -0700

    Update script.js

--- connect/script.js
+++ connect/script.js
@@ -3,10 +3,12 @@ function cdFunction() {
   document.getElementById('connectstart').style.display = "none";
 }
 
-const myTimeout = setTimeout(cdBody, 1000);
+/* const myTimeout = setTimeout(cdBody, 1000);
 
 function cdBody() {
 if (cdFunction() === true) {
   document.getElementById("connect9").style.display = "block"
   document.getElementById("connect10").style.display = "none"
 }
+
+*/
