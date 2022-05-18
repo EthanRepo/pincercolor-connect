29f7d4c3ff559811f67c4e85a2b5846eceefd146
EthanRepo <100090947+EthanRepo@users.noreply.github.com>
2022-05-18 13:00:48 -0700

    Update script.js

--- easy-mode/script.js
+++ easy-mode/script.js
@@ -2,3 +2,12 @@ function startEasy() {
 document.getElementById('easygamestart').style.display = "none";
   document.getElementById('easygaming').style.display = "block";
 }
+function easyCountdown() {
+
+}
+function easyColorScore() {
+
+}
+function easyGameEnd() {
+
+}
