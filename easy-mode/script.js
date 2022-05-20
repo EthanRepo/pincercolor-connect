a9a2a8e3789b82977ceab38f1f23b7e2b9d0bb07
EthanRepo <100090947+EthanRepo@users.noreply.github.com>
2022-05-20 13:03:12 -0700

    Update script.js

--- easy-mode/script.js
+++ easy-mode/script.js
@@ -6,34 +6,49 @@ function startEasy() {
 document.getElementById('easygamestart').style.display = "none";
   document.getElementById('easygaming').style.display = "block";
   function easyGaming() {
-  
+const gamingtimer = setTimeout(easyEnd, 4500);
   }
 }
-function easyCountdown() {
-var timer;
-var count = 60;
-
-$("#counter").text(count);
-//update display
-
-timer = setTimeout(update, 1000);
-//this allows for 'clearTimeout' if needed
-
-function update()
-{
-    if (count > 0)
-    {
-       $("#counter").text(--count);
-       timer = setTimeout(update, 1000);
-    }
-    else
-    {
-      document.getElementById('easygameend').style.display = "block";
-  document.getElementById('easygaming').style.display = "none";
- document.getElementById("endscore").innerHTML = "Score: " + score;
+function easyEnd() {
+document.getElementById('easygameend').style.display = "block";
+document.getElementById('easygaming').style.display = "none";
+    document.getElementById("endscore").innerHTML = "Score: " + score;
     }
   }
 }
-function easyColorScore() {
+function easyScore() {
+var red = document.getElementById("redg");
+var blue = document.getElementById("blueg");
+var green = document.getElementById("yellowg");
+var yellow = document.getElementById("greeng");
 score + 100
+let colorpick = Math.floor((Math.random() * 4) + 1);
+if (colorpick == 1) {
+red.style.display = 'block';
+blue.style.display = 'none';
+green.style.display = 'none';
+yellow.style.display = 'none';
+document.getElementById("colorchosen").innerHTML = "Put the Peg in the Red Square, and then click the button below when done.";
+}
+    else if (colorpick == 2) {
+red.style.display = 'none';
+blue.style.display = 'block';
+green.style.display = 'none';
+yellow.style.display = 'none';
+document.getElementById("colorchosen").innerHTML = "Put the Peg in the Blue Square, and then click the button below when done.";
+    }
+    else if (colorpick == 3) {
+red.style.display = 'none';
+blue.style.display = 'none';
+green.style.display = 'block';
+yellow.style.display = 'none';
+document.getElementById("colorchosen").innerHTML = "Put the Peg in the Green Square, and then click the button below when done.";
+    }
+    else if (colorpick == 4) {
+red.style.display = 'none';
+blue.style.display = 'none';
+green.style.display = 'none';
+yellow.style.display = 'block';
+document.getElementById("colorchosen").innerHTML = "Put the Peg in the Yellow Square, and then click the button below when done.";
+    }
 }
